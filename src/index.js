7cb15e1b8b9b49359c23b3a94fd32df1cac95fbf
Kyle R. Brown <kyle_brown@intuit.com>
2023-07-06 12:03:35 -0600

    playing with scopes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { v4 as uuidv4 } from 'uuid';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { v4 as uuidv4 } from "uuid";
+import * as _ from "lodash";
 export function doSomething() {
   console.log("Inside doSomething()");
-  console.log(React);
-  console.log(ReactDOM);
-  console.log(uuidv4);
+  console.log("react", React);
+  console.log("react-dom", ReactDOM);
+  console.log("uuid", uuidv4);
+  console.log("lodash", _);
 }
 
 doSomething();
